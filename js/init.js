adf5480838d67830cb222890a633a9df3478ac42
proyectoWeb3NV41 <85762144+proyectoWeb3NV41@users.noreply.github.com>
2021-06-11 16:22:46 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,78 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+const firebaseConfig = {
+    // ...
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+var provider = new firebase.auth.GoogleAuthProvider();
+        // Your web app's Firebase configuration
+        var firebaseConfig = {
+          apiKey: "AIzaSyAYw8DERDq30QyKR_1625Y2tp3SKUx-6uE",
+          authDomain: "tienda-online-48f3d.firebaseapp.com",
+          projectId: "tienda-online-48f3d",
+          storageBucket: "tienda-online-48f3d.appspot.com",
+          messagingSenderId: "410289638526",
+          appId: "1:410289638526:web:eaf5f1d2dee104209bdc2f"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+          firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+          firebase.auth()
+  .getRedirectResult()
+  .then((result) => {
+    if (result.credential) {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // ...
+    }
+    // The signed-in user info.
+    var user = result.user;
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
